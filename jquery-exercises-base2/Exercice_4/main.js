205ca6472cbaefdf3ef169640cb63bb8f4ec109b
Wynendaele Jimmy <[jimmy.wynendaele@gmail.com]>
2018-01-21 21:36:17 +0100

    first commit

---
+++ jquery-exercises-base2/Exercice_4/main.js
@@ -0,0 +1,24 @@
+$('#green').click(function(){
+
+      $('#texte').removeClass().addClass('green');
+ 
+
+
+})
+
+
+$('#red').click(function(){
+
+    $('#texte').removeClass().addClass('red');
+      
+
+})
+
+    
+$('#blue').click(function(){
+
+    $('#texte').removeClass().addClass('blue');
+      
+
+})
+
